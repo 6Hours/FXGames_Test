2f3ec5754e0e0173c7fca802248dcc46d4eef019
vendigomania <vendigomania@gmail.com>
2022-09-12 16:02:43 +0300

    Update BaseItemVisualizator.cs

--- Assets/Scripts/View/Visualizators/BaseItemVisualizator.cs
+++ Assets/Scripts/View/Visualizators/BaseItemVisualizator.cs
@@ -12,9 +12,14 @@ namespace View.Visualizators
         {
             if (_item == item) return;
 
-            if (item != null) item.OnChangeItem -= () => UpdateItem(item);
+            if (item != null) item.OnChangeItem -=  OnItemChange;
             item = _item;
-            item.OnChangeItem += () => UpdateItem(item);
+            item.OnChangeItem += OnItemChange;
+        }
+
+        private void OnItemChange()
+        {
+            UpdateItem(Item);
         }
 
         public BaseItem Item => item;
